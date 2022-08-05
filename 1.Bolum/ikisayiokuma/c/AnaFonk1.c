01f573e8caa0b9e49a54f573c42f9dce38c7c1fc
brtsrm <bsarmis@outlook.com>
2022-08-05 12:58:15 +0300

    Algoritmaya başlangıç

---
+++ 1.Bolum/ikisayiokuma/c/AnaFonk1.c
@@ -0,0 +1,10 @@
+int main()
+{
+    int n,m,toplam;
+    float ortalama;
+    printf("iki sayi girin \n");
+    scanf("%d %d",&n,&m);
+    toplam  = n+m;
+    ortalama = toplam/2;
+    printf("Ortalama = %f olarak hesaplandi", ortalama);
+}
---
+++ 1.Bolum/ikisayiokuma/php/index.php
@@ -0,0 +1,14 @@
+<?php
+ 
+
+ // terminalden girilen sayıyı 
+ $sayi1 = (int)readline();
+ $sayi2 = (int)readline();
+
+
+ // ortalama
+ $toplam = (int)$sayi1+$sayi2;
+ $ortalama = $toplam/2;
+ 
+
+ printf("Ortalama = %f olarak hesaplandi",$ortalama);
